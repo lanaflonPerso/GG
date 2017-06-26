7dfc207153ce6284e9a23b7e24d7da80c6b016c5
Guillaume <guillaume.humbert.gh@gmail.com>
2017-06-26 11:37:38 +0200

    fonction 2

--- src/ch/makery/address/view/TauxJournalierRemplissageHotelController.java
+++ src/ch/makery/address/view/TauxJournalierRemplissageHotelController.java
@@ -1,12 +1,23 @@
 package ch.makery.address.view;
 
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import application.Main;
+import ch.makery.address.DAO.ReserverDAO;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
 
 import javafx.fxml.Initializable;
@@ -31,7 +42,101 @@ public class TauxJournalierRemplissageHotelController extends Application implem
 	@FXML
 	private Button b6;
 	
+	@FXML
+	private Button validate;
 	
+	@FXML
+	private LineChart<String, Number> graph;
+	
+	@FXML
+	private DatePicker date1;
+	
+	@FXML
+	private DatePicker date2;
+	
+	
+	
+	public Button getB1() {
+		return b1;
+	}
+
+	public void setB1(Button b1) {
+		this.b1 = b1;
+	}
+
+	public Button getB2() {
+		return b2;
+	}
+
+	public void setB2(Button b2) {
+		this.b2 = b2;
+	}
+
+	public Button getB3() {
+		return b3;
+	}
+
+	public void setB3(Button b3) {
+		this.b3 = b3;
+	}
+
+	public Button getB4() {
+		return b4;
+	}
+
+	public void setB4(Button b4) {
+		this.b4 = b4;
+	}
+
+	public Button getB5() {
+		return b5;
+	}
+
+	public void setB5(Button b5) {
+		this.b5 = b5;
+	}
+
+	public Button getB6() {
+		return b6;
+	}
+
+	public void setB6(Button b6) {
+		this.b6 = b6;
+	}
+
+	public Button getValidate() {
+		return validate;
+	}
+
+	public void setValidate(Button validate) {
+		this.validate = validate;
+	}
+
+	public LineChart<String, Number> getGraph() {
+		return graph;
+	}
+
+	public void setGraph(LineChart<String, Number> graph) {
+		this.graph = graph;
+	}
+
+
+	public DatePicker getDate1() {
+		return date1;
+	}
+
+	public void setDate1(DatePicker date1) {
+		this.date1 = date1;
+	}
+
+	public DatePicker getDate2() {
+		return date2;
+	}
+
+	public void setDate2(DatePicker date2) {
+		this.date2 = date2;
+	}
+
 	public void actionB1(){
 		Main vc = new Main();
 		System.out.println("test b1");
@@ -66,11 +171,51 @@ public class TauxJournalierRemplissageHotelController extends Application implem
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		System.out.println("test");
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+	public void tauxFrequantation(){
+		this.graph.getData().clear();
+		LocalDate localDate =  getDate1().getValue();
+		Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+		Date date_debut  = Date.from(instant);
+		
+		localDate =  getDate2().getValue();
+		instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+		Date date_fin  = Date.from(instant);
+		
+		
+		if((date_fin.getTime()-date_debut.getTime()) / (60*1000*60*24) %60 <= 6 
+				&& date_fin.getTime()-date_debut.getTime() / (60*1000*60*24) %60 >=0){
+			
+			CategoryAxis xAxis = new CategoryAxis();
+	        NumberAxis yAxis = new NumberAxis();
+	        BarChart<String,Number> bc = 
+	            new BarChart<>(xAxis,yAxis);
+	        XYChart.Series series1 = new XYChart.Series();   
+			for (LocalDate date =  getDate1().getValue(); date.isBefore(getDate2().getValue().plusDays(1)); date = date.plusDays(1))
+			{
+				
+				ReserverDAO rs = new ReserverDAO();
+				System.out.println(date.toString());
+				series1.getData().add(new XYChart.Data(date.toString(),  rs.getReserverByDate(date.toString())));   
+				
+			}
+			
+	        
+	           
+	        bc.getData().add(series1);
+			this.graph.getData().addAll(bc.getData());
+			
+			
+			
+			
+		}
+		
 		
 	}
 
