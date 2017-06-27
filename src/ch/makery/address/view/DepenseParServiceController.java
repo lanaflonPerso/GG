d05f569dd99adaa7fe56c7c913c436ddd257d5f6
Guillaume <guillaume.humbert.gh@gmail.com>
2017-06-27 11:27:46 +0200

    fonction 4

--- src/ch/makery/address/view/DepenseParServiceController.java
+++ src/ch/makery/address/view/DepenseParServiceController.java
@@ -1,11 +1,23 @@
 package ch.makery.address.view;
 
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import application.Main;
+import ch.makery.address.DAO.ReserverDAO;
+import ch.makery.address.DAO.ServiceDAO;
+import ch.makery.address.model.Service;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -31,7 +43,66 @@ public class DepenseParServiceController extends Application implements Initiali
 	@FXML
 	private Button b6;
 	
+	@FXML
+	private BarChart<String, Number> graph;
 	
+	
+	public Button getB1() {
+		return b1;
+	}
+
+	public void setB1(Button b1) {
+		this.b1 = b1;
+	}
+
+	public Button getB2() {
+		return b2;
+	}
+
+	public void setB2(Button b2) {
+		this.b2 = b2;
+	}
+
+	public Button getB3() {
+		return b3;
+	}
+
+	public void setB3(Button b3) {
+		this.b3 = b3;
+	}
+
+	public Button getB4() {
+		return b4;
+	}
+
+	public void setB4(Button b4) {
+		this.b4 = b4;
+	}
+
+	public Button getB5() {
+		return b5;
+	}
+
+	public void setB5(Button b5) {
+		this.b5 = b5;
+	}
+
+	public Button getB6() {
+		return b6;
+	}
+
+	public void setB6(Button b6) {
+		this.b6 = b6;
+	}
+
+	public BarChart<String, Number> getGraph() {
+		return graph;
+	}
+
+	public void setGraph(BarChart<String, Number> graph) {
+		this.graph = graph;
+	}
+
 	public void actionB1(){
 		Main vc = new Main();
 		System.out.println("test b1");
@@ -66,9 +137,25 @@ public class DepenseParServiceController extends Application implements Initiali
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		System.out.println("test");
+	        ServiceDAO sr = new ServiceDAO();
+			ArrayList<Service> list = (ArrayList<Service>) sr.getService();
+			CategoryAxis xAxis = new CategoryAxis();
+	        NumberAxis yAxis = new NumberAxis();
+	        BarChart<String,Number> bc = 
+	            new BarChart<>(xAxis,yAxis);
+	        XYChart.Series series1 = new XYChart.Series();   
+			for (Service serv : list)
+			{
+				System.out.println(serv.getNom_service() +" / "+serv.getMontant());
+				series1.getData().add(new XYChart.Data(serv.getNom_service(), serv.getMontant()));
+			}
+			bc.getData().add(series1);
+			this.graph.getData().addAll(series1);
 	}
 	
+	public void visualisation(){
+		
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
