db944eaa706c83490e1c93b68897c6260b02253a
Guillaume <guillaume.humbert.gh@gmail.com>
2017-06-27 12:03:06 +0200

    fonction 5

--- src/ch/makery/address/DAO/ServiceEmployeDAO.java
+++ src/ch/makery/address/DAO/ServiceEmployeDAO.java
@@ -2,17 +2,52 @@ package ch.makery.address.DAO;
 
 import application.MySQLSSHConnector;
 import ch.makery.address.model.Metier;
+import ch.makery.address.model.Service;
 import ch.makery.address.model.Service_employe;
 import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
  * @author maxim
  */
 public class ServiceEmployeDAO {
+	
+	public HashMap<String,Integer> getNombreEmpService(){
+        MySQLSSHConnector db = new MySQLSSHConnector();
+            try {
+                Connection connection = db.connection_db();
+		PreparedStatement pr = null;
+		pr = (PreparedStatement) connection.prepareStatement("Select count(emp.id_employe), ser.nom_service"
+				+ " from Employe emp JOIN Metier m ON m.id_metier =emp.id_metier"
+				+ " JOIN Appartenir a ON a.id_metier = m.id_metier"
+				+ " JOIN Service_employe ser ON ser.id_service_employe = a.id_service_employe "
+				+ " GROUP BY ser.id_service_employe");
+		ResultSet rs = pr.executeQuery();
+		HashMap<String,Integer> list = new HashMap<>();
+		while(rs.next()){
+					
+                    list.put(rs.getString(2),rs.getInt(1));
+                    
+		}
+		connection.close();	
+        db.CloseSSHConnection();
+		return list;
+			
+            } catch (SQLException e) {
+		// TODO Auto-generated catch block
+                db.CloseSSHConnection();
+		e.printStackTrace();
+            }
+            
+            db.CloseSSHConnection();
+          return null;
+    }
     public Service_employe getServiceEmployeById(int id){
         MySQLSSHConnector db = new MySQLSSHConnector();
             try {
--- src/ch/makery/address/view/EmployeParServiceController.java
+++ src/ch/makery/address/view/EmployeParServiceController.java
@@ -1,11 +1,21 @@
 package ch.makery.address.view;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import application.Main;
+import ch.makery.address.DAO.ServiceDAO;
+import ch.makery.address.DAO.ServiceEmployeDAO;
+import ch.makery.address.model.Service;
+import ch.makery.address.model.Service_employe;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -31,7 +41,66 @@ public class EmployeParServiceController extends Application implements Initiali
 	@FXML
 	private Button b6;
 	
+	@FXML
+	private BarChart<String, Number> graph;
 	
+	
+	public Button getB1() {
+		return b1;
+	}
+
+	public void setB1(Button b1) {
+		this.b1 = b1;
+	}
+
+	public Button getB2() {
+		return b2;
+	}
+
+	public void setB2(Button b2) {
+		this.b2 = b2;
+	}
+
+	public Button getB3() {
+		return b3;
+	}
+
+	public void setB3(Button b3) {
+		this.b3 = b3;
+	}
+
+	public Button getB4() {
+		return b4;
+	}
+
+	public void setB4(Button b4) {
+		this.b4 = b4;
+	}
+
+	public Button getB5() {
+		return b5;
+	}
+
+	public void setB5(Button b5) {
+		this.b5 = b5;
+	}
+
+	public Button getB6() {
+		return b6;
+	}
+
+	public void setB6(Button b6) {
+		this.b6 = b6;
+	}
+
+	public BarChart<String, Number> getGraph() {
+		return graph;
+	}
+
+	public void setGraph(BarChart<String, Number> graph) {
+		this.graph = graph;
+	}
+
 	public void actionB1(){
 		Main vc = new Main();
 		System.out.println("test b1");
@@ -66,12 +135,26 @@ public class EmployeParServiceController extends Application implements Initiali
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		System.out.println("test");
+	        ServiceEmployeDAO sr = new ServiceEmployeDAO();
+	        HashMap<String,Integer> list =sr.getNombreEmpService();
+			CategoryAxis xAxis = new CategoryAxis();
+	        NumberAxis yAxis = new NumberAxis();
+	        BarChart<String,Number> bc = 
+	            new BarChart<>(xAxis,yAxis);
+	        XYChart.Series series1 = new XYChart.Series();   
+			for (String serv : list.keySet())
+			{
+				series1.getData().add(new XYChart.Data(serv,list.get(serv)));
+			}
+			bc.getData().add(series1);
+			this.graph.getData().addAll(series1);
 	}
 	
+	public void visualisation(){
+		
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
 	}
-
 }
