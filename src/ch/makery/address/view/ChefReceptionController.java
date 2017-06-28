a7d7cc5876d15ce6fea2e9994bf1d606415d16c9
RomainMarcq <romainmarcq@yahoo.fr>
2017-06-28 17:12:58 +0200

    boutton deconnexion controller

--- src/ch/makery/address/view/ChefReceptionController.java
+++ src/ch/makery/address/view/ChefReceptionController.java
@@ -11,15 +11,22 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class ChefReceptionController extends Application implements Initializable{
-	
+
 	@FXML
 	private Button b1;
-	
+
 	@FXML
 	private Button b2;
-	
-	
-	
+
+	@FXML
+	private Button BD;
+
+
+	public void actionBD(){
+		Main vc = new Main();
+		vc.setScene("ListeOverview.fxml");
+	}
+
 	public Button getB1() {
 		return b1;
 	}
@@ -36,26 +43,26 @@ public class ChefReceptionController extends Application implements Initializabl
 		this.b2 = b2;
 	}
 
-	
+
 
 	public void actionB1(){
 		Main vc = new Main();
 		vc.setScene("EncaisserClients.fxml");
 	}
-	
+
 	public void actionB2(){
 		Main vc = new Main();
 		vc.setScene("PlanningReservation.fxml");
 	}
-	
-	
-	
-	
+
+
+
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 	}
 }
