90512febdf8026c2ef10b8bb5baf75f0e2d77a18
MaximeJack <maxime_jacquet@orange.fr>
2017-06-28 16:07:08 +0200

    CentralesReservation Controller / recup des donnees

--- src/ch/makery/address/DAO/OffreDAO.java
+++ src/ch/makery/address/DAO/OffreDAO.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  * @author maxim
  */
 public class OffreDAO {
-    public Offre getCentraleById(int id){
+    public Offre getOffreById(int id){
         MySQLSSHConnector db = new MySQLSSHConnector();
             try {
                 Connection connection = db.connection_db();
@@ -153,6 +153,40 @@ public class OffreDAO {
                 Connection connection = db.connection_db();
 		PreparedStatement pr = null;
 		pr = (PreparedStatement) connection.prepareStatement("Select * from Offre");
+		ResultSet rs = pr.executeQuery();
+                ArrayList<Offre> tabOffres = new ArrayList<>();
+                
+		while (rs.next()){
+                    Offre res = new Offre();
+                    res.setId_offre(rs.getInt("id_offre"));
+                    res.setId_chambre(rs.getInt("id_chambre"));
+                    res.setId_centrale_reservation(rs.getInt("id_centrale_reservation"));
+                    res.setId_agence_voyage(rs.getInt("id_agence_voyage"));
+                    res.setLien_url(rs.getString("lien_url"));
+                    res.setDesc_offre(rs.getString("desc_offre"));
+                    res.setNb_visites(rs.getInt("nb_visites"));
+                    
+                    connection.close();
+		    db.CloseSSHConnection();
+                    tabOffres.add(res);
+		}
+                return tabOffres;
+			
+            } catch (SQLException e) {
+		// TODO Auto-generated catch block
+                db.CloseSSHConnection();
+		e.printStackTrace();
+            }
+            
+        db.CloseSSHConnection();
+        return null;
+    }
+    public ArrayList<Offre> getAllCentrale(){
+        MySQLSSHConnector db = new MySQLSSHConnector();
+            try {
+                Connection connection = db.connection_db();
+		PreparedStatement pr = null;
+		pr = (PreparedStatement) connection.prepareStatement("Select * from Offre WHERE id_centrale_reservation NOT NULL");
 		ResultSet rs = pr.executeQuery();
                 ArrayList<Offre> tabOffres = new ArrayList<>();
                 
